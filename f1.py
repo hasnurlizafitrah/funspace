309ca9af7a7c86e23ad1fa62e144e44508ca0d2d
hasnurlizafitrah <hasnurlizafitrah201104@gmail.com>
2023-02-01 14:01:32 +0800

    luas segitiga

---
+++ f1.py
@@ -0,0 +1,17 @@
+def lu9as_segitiga():
+    a = int(input("masukkan alas segitiga : "))
+    t = int(input("masukkan tinggi segitiga: "))
+    luas = a * t /2
+    print("Luas segitiga adalah : ", luas)
+
+    luas_segitiga()
+
+
+    # Hitung luas Persegi Panjang
+    def luas_persegi_panjang();
+        p = int (input("masukkan panjang persegi : "))
+        l = int (input("masukkan lebar persegi : "))
+
+        luas = p * l
+        print("luas persegi adalah : luas ")
+    luas_persegi_panjang()
